0df8605723f9bd8d02c4b84ef4be438857643f60
wahidikqbal <wahidikqbal@gmail.com>
2026-04-04 16:05:58 +0700

    menambahkan seeder

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,39 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias Appsatu.Blog.Category
+alias Appsatu.Blog.Tag
+alias Appsatu.Repo
+
+category_names = [
+  "Technology",
+  "Business",
+  "Lifestyle",
+  "Education",
+  "Travel"
+]
+
+tag_names = [
+  "elixir",
+  "phoenix",
+  "ecto",
+  "liveview",
+  "tutorial"
+]
+
+Enum.each(category_names, fn name ->
+  Repo.insert!(
+    %Category{name: name},
+    on_conflict: :nothing,
+    conflict_target: :name
+  )
+end)
+
+Enum.each(tag_names, fn name ->
+  Repo.insert!(
+    %Tag{name: name},
+    on_conflict: :nothing,
+    conflict_target: :name
+  )
+end)
